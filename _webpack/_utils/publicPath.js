c5bcf789df7c1a806e394202e43ea0e3f27f31c0
Cristobal Dominguez <cristobald@gmail.com>
2019-08-22 15:08:09 -0400

    Small Improvements

--- _webpack/_utils/publicPath.js
+++ _webpack/_utils/publicPath.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = (folder, prefix = '') => {
-  const theme = path.basename(path.resolve('../'));
-  return `${prefix}/wp-content/themes/${theme}/${folder}/`;
+  const theme = path.basename(path.resolve('../'))
+  return `${prefix}/wp-content/themes/${theme}/${folder}/`
 }
